cf1fc0d27674fecf669049420c92fd655a996d4f
fanzk <fanzk@info2soft.com>
2021-01-25 18:43:16 +0800

    fix regular expression

--- src/main/java/com/string/testString.java
+++ src/main/java/com/string/testString.java
@@ -1,5 +1,6 @@
 package com.string;
 
+import com.google.common.collect.Lists;
 import info2soft.qa.common.util.StringUtil;
 import org.junit.jupiter.api.Test;
 
@@ -46,7 +47,8 @@ public class testString {
 
 	@Test
 	public void test3(){
-		String str = "2020-07-17 13:42:26.33892";
+		String str
+				= "2020-07-17 13:42:26.33892";
 		int index = str.indexOf('.');
 		System.out.println(str.substring(0,index));
 	}
@@ -135,5 +137,51 @@ public class testString {
 			}
 		}
 		return cmd;
+	}
+
+
+	@Test
+	public void test5(){
+		String str = "\u001B[A\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[C\u001B[K'\n" +
+				"2feab27be4c164f1f9e814293431073b";
+		String [] strs = str.split("\n");
+		List<String> lists = StringUtil.toStringList(strs);
+		Iterator<String> iterator = lists.iterator();
+		Pattern pattern = Pattern.compile("[0-9a-zA_Z]{32}");
+		while (iterator.hasNext()){
+			String x = iterator.next().trim();
+			if(!pattern.matcher(x).find()){
+				//iterator.remove();
+				lists.remove(x);
+			}
+		}
+		System.out.println(lists.size());
+	}
+
+	@Test
+	public void test7(){
+		String str ="6.7";
+		boolean result = str.contains("6.");
+		System.out.println(result);
+
+	}
+
+	@Test
+	public void test6(){
+		String out ="aa_0.txt,aa_10.txt,aa_11.txt,aa_12.txt,aa_13.txt,aa_14.txt,aa_15.txt,aa_16.txt,aa_17.txt,aa_18.txt,aa_19.txt,aa_1.txt,aa_20.txt,aa_21.txt,aa_22.txt,aa_23.txt,aa_24.txt,aa_25.txt,aa_26.txt,aa_27.txt,aa_28.txt,aa_29.txt,aa_2.txt,aa_30.txt,aa_31.txt,aa_32.txt,aa_33.txt,aa_34.txt,aa_35.txt,aa_36.txt,aa_37.txt,aa_38.txt,aa_39.txt,aa_3.txt,aa_40.txt,aa_41.txt,aa_42.txt,aa_43.txt,aa_44.txt,aa_45.txt,aa_46.txt,aa_47.txt,aa_48.txt,aa_49.txt,aa_4.txt,aa_5.txt,aa_6.txt,aa_7.txt,aa_8.txt,aa_9.txt";
+		String[] fileNamesArray = out.split(",");
+		Arrays.sort(fileNamesArray);
+		out = StringUtil.toPlainText(fileNamesArray,",");
+		System.out.println("one: "+out);
+
+		String out1 ="aa_0.txt,aa_1.txt,aa_10.txt,aa_11.txt,aa_12.txt,aa_13.txt,aa_14.txt,aa_15.txt,aa_16.txt,aa_17.txt,aa_18.txt,aa_19.txt,aa_2.txt,aa_20.txt,aa_21.txt,aa_22.txt,aa_23.txt,aa_24.txt,aa_25.txt,aa_26.txt,aa_27.txt,aa_28.txt,aa_29.txt,aa_3.txt,aa_30.txt,aa_31.txt,aa_32.txt,aa_33.txt,aa_34.txt,aa_35.txt,aa_36.txt,aa_37.txt,aa_38.txt,aa_39.txt,aa_4.txt,aa_40.txt,aa_41.txt,aa_42.txt,aa_43.txt,aa_44.txt,aa_45.txt,aa_46.txt,aa_47.txt,aa_48.txt,aa_49.txt,aa_5.txt,aa_6.txt,aa_7.txt,aa_8.txt,aa_9.txt";
+		String[] fileNamesArray1 = out.split(",");
+		Arrays.sort(fileNamesArray1);
+		out1 = StringUtil.toPlainText(fileNamesArray1,",");
+		System.out.println("second: "+out1);
+
+		System.out.println(out.equals(out1));
+		System.out.println();
+
 	}
 }
---
+++ src/test/java/com/regular/TestRegular02.java
@@ -0,0 +1,156 @@
+package com.regular;
+
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author fanzk
+ * @version 1.8
+ * @date 2021/1/25 16:39
+ */
+public class TestRegular02 {
+	@Test
+	public void test1(){
+		String Regex = "\\w(\\d\\d)(\\w+)";
+		String TestStr = "A22happy11";
+		String TestStr2 = "f88kang222";
+		Pattern p = Pattern.compile(Regex);
+		Matcher matcher = p.matcher(TestStr);
+		if(matcher.find()){
+			System.out.println("group():"+matcher.group());
+			int gc = matcher.groupCount();
+			System.out.println(gc);
+			for (int i=0;i<=gc;i++){
+				System.out.println("group "+i+" :"+matcher.group(i));
+			}
+		}
+
+
+		System.out.println("*********************");
+		matcher.reset(TestStr2);
+		if(matcher.find()){
+			System.out.println("group():"+matcher.group());
+			int gc = matcher.groupCount();
+			System.out.println(gc);
+			for (int i=0;i<=gc;i++){
+				System.out.println("group "+i+" :"+matcher.group(i));
+			}
+		}
+
+	}
+
+	@Test
+	public void testStart(){
+		//创建一个Matcher,使用Matcher.start()方法
+		String candidateString = "My name is Bond. James Bond.";
+		System.out.println("candidateString 的length = "+candidateString);
+		System.out.println("candidateString的第一个Bond 开始的下标："+candidateString.indexOf("Bond"));
+		System.out.println("candidateString的最后一个Bond 开始的下标："+candidateString.lastIndexOf("Bond"));
+		String matcherHelper[] = {"          ^","                      ^"};
+		Pattern p = Pattern.compile("Bond");
+		Matcher matcher = p.matcher(candidateString);
+		//找到第一个‘bond’的开始下标
+        matcher.find();
+        int startIndex = matcher.start();
+		System.out.println(candidateString);
+		System.out.println(matcherHelper[0]+startIndex);
+		//找到第二个‘bond’的开始下标
+		matcher.find();
+		int nextIndex = matcher.start();
+		System.out.println(candidateString);
+		System.out.println(matcherHelper[1]+nextIndex);
+	}
+
+	@Test
+	public  void testGroup(){
+		//创建一个Pattern
+		Pattern p = Pattern.compile("Bond");
+		//创建一个Matcher, 以便使用group()方法
+		String candidateString = "My name is Bond. James Bond.";
+		Matcher matcher = p.matcher(candidateString);
+		matcher.find();
+		System.out.println(String.format("group匹配的字符串 : %s",matcher.group()));
+		System.out.println(String.format("匹配的开始位置 : %d", matcher.start()));
+		System.out.println(String.format("匹配的结束位置 : %d", matcher.end()));
+
+		System.out
+				.println("---再次使用matcher.find()方法，看看matcher中group、start、end方法的效果");
+		matcher.find();
+		System.out.println(String.format("group匹配的字符串 : %s",matcher.group()));;
+		System.out.println(String.format("匹配的开始位置 : %d", matcher.start()));
+		System.out.println(String.format("匹配的结束位置 : %d", matcher.end()));
+		System.out.println(String.format("candidateString字符串的长度 : %d", candidateString.length()));
+
+	}
+
+
+	@Test
+	public void testID_card(){
+		//测试是否为合法的身份证号
+		String[] strs = { "130681198712092019", "13068119871209201x",
+				"13068119871209201", "123456789012345", "12345678901234x",
+				"1234567890123" };
+		//准备正则表达式（身份证有15位和18位这两种，身份证的最后一位可能是字母）
+		String regex ="(\\d{14}\\w)|(\\d{17}\\w)";
+		//准备开始匹配，判断所有的输入是否是正确
+		Pattern regular = Pattern.compile(regex); //创建匹配的规则Patter
+		StringBuilder sb = new StringBuilder();
+		//遍历所有要匹配的字符串
+		for (int i = 0; i < strs.length; i++) {
+			Matcher matcher = regular.matcher(strs[i]); //创建一个Matcher
+			sb.append("身份证： ");
+			sb.append(strs[i]);
+			sb.append("  匹配：");
+			sb.append(matcher.matches());
+			System.out.println(sb.toString());
+			sb.delete(0,sb.length()); //清空StringBuilder的方法
+
+		}
+
+		GetBirthDay(strs);
+
+
+	}
+
+	private void GetBirthDay(String[] strs){
+		System.out.println("准备开始获取出生日期");
+		//准备验证规则
+		Pattern BirthDayRegular = Pattern.compile("(\\d{6})(\\d{8})(.*)");
+		// .*连在一起就意味着任意数量的不包含换行的字符
+		Pattern YearMonthDayRegular = Pattern
+				.compile("(\\d{4})(\\d{2})(\\d{2})");
+		for (int i = 0; i < strs.length; i++) {
+			Matcher matcher = BirthDayRegular.matcher(strs[i]);
+			if(matcher.matches()){
+				Matcher matcher1 = YearMonthDayRegular.matcher(matcher.group(2));
+				if(matcher1.matches()){
+					System.out.println(strs[i]+"中的出生年月日分解为：年->"+matcher1.group(1)+" 月->"+matcher1.group(2)+" 日->"+matcher1.group(3));
+				}
+			}
+
+		}
+	}
+
+
+	@Test
+	public void testMatchers(){
+		String Regex = "\\w(\\d\\d)(\\w+)";
+		String TestStr = "A22happy11";
+		Pattern p = Pattern.compile(Regex);
+		Matcher matcher = p.matcher(TestStr);
+
+		if(matcher.find()){
+			System.out.println("group():"+matcher.group());
+			int gc = matcher.groupCount();
+			System.out.println(gc);
+			for (int i=0;i<=gc;i++){
+				System.out.println("group "+i+" :"+matcher.group(i));
+			}
+		}
+		System.out.println(matcher.matches());
+
+	}
+
+}
