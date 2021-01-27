84b13a493e4302e05e03c3b5ad16e02d9abb1f74
fanzk <fanzk@info2soft.com>
2021-01-27 18:01:33 +0800

    commit test1

---
+++ src/test/java/com/log/TestLog.java
@@ -0,0 +1,15 @@
+package com.log;
+
+import org.junit.Test;
+
+/**
+ * @author fanzk
+ * @version 1.8
+ * @date 2021/1/27 17:36
+ */
+public class TestLog {
+	@Test
+	public void test1(){
+		System.out.println("test1()");
+	}
+}
