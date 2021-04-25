fe8c255424a635298ea112e33a8e9609b875cbb9
fanzk <fanzk@info2soft.com>
2021-04-25 09:19:11 +0800

    add getResourceAsStream

--- src/main/java/com/sax/Main.java
+++ src/main/java/com/sax/Main.java
@@ -15,7 +15,10 @@ import java.io.InputStream;
  */
 public class Main {
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        //path 不以’/'开头时默认是从此类所在的包下取资源，以’/'开头则是从ClassPath根下获取。其只是通过path构造一个绝对路径，最终还是由ClassLoader获取资源
         InputStream inputStream = Main.class.getResourceAsStream("/book.xml");
+        //默认则是从ClassPath根下获取，path不能以’/'开头，最终是由ClassLoader获取资源。
+       // InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("book.xml");
         SAXParserFactory spf = SAXParserFactory.newInstance();
         SAXParser saxParser = spf.newSAXParser();
         saxParser.parse(inputStream, new MyHandler());
