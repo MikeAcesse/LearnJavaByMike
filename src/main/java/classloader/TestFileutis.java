ef58ee3d1d17f312232f769632996aecc1bac655
fanzk <fanzk@info2soft.com>
2021-04-26 14:10:17 +0800

    analysis ClassLoader

---
+++ src/main/java/classloader/TestFileutis.java
@@ -0,0 +1,12 @@
+package classloader;
+
+/**
+ * @author fanzk
+ * @version 1.8
+ * @date 2021/4/26 11:22
+ */
+public class TestFileutis {
+	public static void main(String[] args) {
+		FileUtis.test("D:\\lib\\Test.class");
+	}
+}
---
+++ src/main/java/com/clazz/Test.java
@@ -0,0 +1,9 @@
+package com.clazz;
+
+/**
+ * @author fanzk
+ * @version 1.8
+ * @date 2021/4/26 9:28
+ */
+public class Test {
+}
