1baf265248ada73d25dd2268f1e4dedf706fbc22
fanzk <fanzhikang2004@163.com>
2021-04-27 20:27:25 +0800

    add reflect

---
+++ src/main/java/ssm/com/chapter2/proxy/HelloWorldImpl.java
@@ -0,0 +1,8 @@
+package ssm.com.chapter2.proxy;
+
+public class HelloWorldImpl implements HelloWorld {
+    @Override
+    public void sayHelloWorld() {
+        System.out.println("Hello World");
+    }
+}
