51828bf498ab031a82d77146d55e5617a19b5578
fanzk <fanzk@info2soft.com>
2021-08-13 11:31:26 +0800

    csdn 中的适配器模式

---
+++ src/main/java/com/pattern/csdn/adapter/Bird.java
@@ -0,0 +1,15 @@
+package com.pattern.csdn.adapter;
+
+/**
+ * @author fanzk
+ * @version 1.8
+ * @date 2021/8/13 11:11
+ */
+public class Bird {
+	public void jiji(){
+		System.out.println("唧唧。。。。。。");
+	}
+	public void fly(){
+		System.out.println("我在飞。。。。。");
+	}
+}
