1b665b4b35503c9c5ab9e50e765397044f40bcea
fanzk <fanzhikang2004@163.com>
2021-08-15 19:19:58 +0800

    03 获取当前执行任务的线程

---
+++ src/main/java/imooc/rens/part03/Main.java
@@ -0,0 +1,8 @@
+package imooc.rens.part03;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(Thread.currentThread());
+    }
+}
