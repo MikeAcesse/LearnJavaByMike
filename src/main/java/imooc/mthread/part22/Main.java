352d3063f6d4ef9967e12e3774dd618a14ff56c4
fanzk <fanzk@info2soft.com>
2021-08-27 09:15:45 +0800

    22：非阻塞式获取锁

---
+++ src/main/java/imooc/mthread/part22/Main.java
@@ -0,0 +1,19 @@
+package imooc.mthread.part22;
+
+/**
+ * @author fanzk
+ * @version 1.8
+ * @date 2021/8/26 18:05
+ */
+public class Main {
+	public static void main(String[] args) {
+		//创建任务
+		Task task = new Task();
+		//创建线程
+		Thread thread1  = new Thread(task);
+		Thread thread2 = new Thread(task);
+		//启动线程
+		thread1.start();
+		thread2.start();
+	}
+}
